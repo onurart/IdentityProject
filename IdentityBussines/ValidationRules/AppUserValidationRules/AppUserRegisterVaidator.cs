c1548982a370aea03def34808d929cd43bc21c4c
Onur Umutluoğlu <onurumutluoglu@gmail.com>
2023-12-02 00:35:03 +0300

    sssss

---
+++ IdentityBussines/ValidationRules/AppUserValidationRules/AppUserRegisterVaidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using IdentityDtoLayer.Dtos.AppUserDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityBussines.ValidationRules.AppUserValidationRules
+{
+    public class AppUserRegisterVaidator:AbstractValidator<AppserRegisterDtos>
+    {
+        public AppUserRegisterVaidator()
+        {
+            RuleFor(n => n.Name).NotEmpty().WithMessage("Ad Alanı Boş Geçilemez");
+            RuleFor(n => n.SurName).NotEmpty().WithMessage("Soyad Alanı Boş Geçilemez");
+            RuleFor(n => n.UserName).NotEmpty().WithMessage("Ad Alanı Boş Geçilemez");
+            RuleFor(n => n.Email).NotEmpty().WithMessage("kullanıcı Alanı Boş Geçilemez");
+            RuleFor(n => n.Password).NotEmpty().WithMessage("Şifre Alanı Boş Geçilemez");
+            RuleFor(n => n.ConfirmPassword).NotEmpty().WithMessage("Şifre tekrar Alanı Boş Geçilemez");
+            RuleFor(n => n.Name).MaximumLength(30).WithMessage("En 30 Karakter  girin");
+            RuleFor(n => n.Name).MaximumLength(30).WithMessage("En 2 Karakter  girin");
+            RuleFor(n => n.Email).EmailAddress().WithMessage("Ad Alanı Boş Geçilemez");
+        }
+    }
+}
---
+++ IdentityDataAccesLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityDataAccesLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    { 
+        void Insert(T t);
+        void Delete(T t);
+        void Update(T t);
+        void GetBuID(T t);
+        void GetList(T t);
+        
+    }
+}
